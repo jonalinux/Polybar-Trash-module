e3aa2f0f80975fd265c09127d8a1bf886483153d
Jonathan Sanfilippo <103053714+JonathanSanfilippo@users.noreply.github.com>
2022-04-27 19:05:42 +0100

    Add files via upload

--- info-trash.sh
+++ info-trash.sh
@@ -1,29 +1,22 @@
 #!/bin/bash
 
+get_trash() { 
+CHECK=$(du -a $HOME/.local/share/Trash/files/ | wc -l );
+TRASH=$(expr "$CHECK" - 1); 
+SIZE=$(du -sh $HOME/.local/share/Trash/files | awk '{ printf $1}');
+EFFECTIVE_SIZE=$(du -s $HOME/.local/share/Trash/files | awk '{ printf $1}');
+LIMIT=$(printf 5000000);
+ }
+
+get_trash
 
 
+if (("$EFFECTIVE_SIZE" > "$LIMIT" )); then
+    echo "%{F#ff5770}%{F#f2f2f2}Very Full %{F#77f2f2f2}$SIZE"
+   elif (("$TRASH" > 0)); then
+   echo "%{F#FF8B42} %{F#f2f2f2}Full %{F#77f2f2f2}$SIZE"
+else  
+   echo "%{F#5DC0FE}%{F#f2f2f2}Empty %{F#77f2f2f2}"      
+fi 
 
-get_trash() { TRASH=$(du -a $HOME/.local/share/Trash/files/* 2>/dev/null | wc -l); }
 
-
-while true; do
- get_trash
-
-while (( TRASH > 0 )); do
-        if (( TRASH  == 1 )); then
-            echo "%{F#fc1732} %{F#f2f2f2}$TRASH Files"
-        elif (( TRASH  > 1 )); then
-            echo "%{F#fc1732} %{F#f2f2f2}$TRASH Files"
-        else
-            echo "%{F#5DC0FE} %{F#f2f2f2}$TRASH Files"
-        fi
-        sleep 10
-        get_trash
-    done
-    
-    while (( TRASH == 0 )); do
-        echo "%{F#5DC0FE} %{F#f2f2f2}$TRASH Files"
-        sleep 10
-        get_trash
-    done
-done
